0f61eb5c548dccecc2895767a63dd0e4b29c5c6d
somesh4747 <somesh47.karmakar@gmail.com>
2024-08-19 19:07:59 +0530

    project ideas tranformed into divs from Cards...

--- components/about-ideas.tsx
+++ components/about-ideas.tsx
@@ -5,15 +5,15 @@ import Image from 'next/image'
 export default function AboutIdeas() {
     return (
         <div className="max-w-[900px] gap-2 grid grid-cols-12 grid-rows-2 px-8">
-            <Card className="col-span-12 sm:col-span-4 h-[300px] tracking-wide">
-                <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            <div className="col-span-12 sm:col-span-4 h-[300px] tracking-wide rounded-2xl relative overflow-hidden">
+                <div className="absolute z-[100] top-1 flex-col !items-start py-4 px-4">
                     <p className="text-tiny text-white/60 uppercase font-bold">
                         What to Create
                     </p>
                     <h4 className="text-white font-medium text-2xl">
                         Open Innvovation
                     </h4>
-                </CardHeader>
+                </div>
                 <Image
                     width={400}
                     height={400}
@@ -21,16 +21,16 @@ export default function AboutIdeas() {
                     className="z-0 w-full h-full object-cover"
                     src="/ideas/brain.png"
                 />
-            </Card>
-            <Card className="col-span-12 sm:col-span-4 h-[300px]">
-                <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            </div>
+            <div className="col-span-12 sm:col-span-4 h-[300px] relative rounded-2xl overflow-hidden">
+                <div className="absolute z-[100] top-1 flex-col !items-start py-4 px-4">
                     <p className="text-tiny text-white/60 uppercase font-bold">
                         AI/ML
                     </p>
                     <h4 className="text-white font-medium text-2xl capitalize">
                         artificial intelligence &amp; machine learning
                     </h4>
-                </CardHeader>
+                </div>
                 <Image
                     width={400}
                     height={400}
@@ -38,16 +38,16 @@ export default function AboutIdeas() {
                     className="z-0 w-full h-full object-cover"
                     src="/ideas/ai.jpg"
                 />
-            </Card>
-            <Card className="col-span-12 sm:col-span-4 h-[300px]">
-                <CardHeader className="absolute z-10 top-1 flex-col !items-start">
+            </div>
+            <div className="col-span-12 sm:col-span-4 h-[300px] relative rounded-2xl overflow-hidden">
+                <div className="absolute z-[100] top-1 flex-col !items-start py-4 px-4">
                     <p className="text-tiny text-white/60 uppercase font-bold">
                         Health-care
                     </p>
                     <h4 className="text-white font-medium text-2xl">
                         Digital Healthcare Platforms
                     </h4>
-                </CardHeader>
+                </div>
                 <Image
                     width={400}
                     height={400}
@@ -55,19 +55,19 @@ export default function AboutIdeas() {
                     className="z-0 w-full h-full object-cover"
                     src="/ideas/health.jpg"
                 />
-            </Card>
-            <Card
-                isFooterBlurred
-                className="w-full h-[300px] col-span-12 sm:col-span-5"
+            </div>
+            <div
+                
+                className="w-full h-[300px] col-span-12 sm:col-span-5 relative rounded-2xl overflow-hidden"
             >
-                <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                <div className="absolute z-[100] top-1 flex-col !items-start py-4 px-4">
                     <p className="text-tiny text-black/60 uppercase font-bold">
                         Web
                     </p>
                     <h4 className="text-black font-medium text-2xl">
                         Full stack Development
                     </h4>
-                </CardHeader>
+                </div>
                 <Image
                     width={400}
                     height={400}
@@ -75,19 +75,19 @@ export default function AboutIdeas() {
                     className="z-0 w-full h-full scale-125 -translate-y-6 object-cover"
                     src="/ideas/web2.jpg"
                 />
-            </Card>
-            <Card
-                isFooterBlurred
-                className="w-full h-[300px] col-span-12 sm:col-span-7"
+            </div>
+            <div
+                
+                className="w-full h-[300px] col-span-12 sm:col-span-7 relative rounded-2xl overflow-hidden"
             >
-                <CardHeader className="absolute z-10 top-1 flex-col items-start">
+                <div className="absolute z-[100] top-1 flex-col !items-start py-4 px-4">
                     <p className="text-tiny text-white/60 uppercase font-bold">
                         Ed - tech
                     </p>
                     <h4 className="text-white/90 font-medium text-2xl">
                         Create your Ed-tech Platform
                     </h4>
-                </CardHeader>
+                </div>
                 <Image
                     width={400}
                     height={400}
@@ -95,7 +95,7 @@ export default function AboutIdeas() {
                     className="z-0 w-full h-full object-cover"
                     src="/ideas/edu.jpg"
                 />
-            </Card>
+            </div>
         </div>
     )
 }
