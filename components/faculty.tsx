94fa6c9e9f826a5974c18df5dec6b666ccd85758
somesh4747 <somesh47.karmakar@gmail.com>
2024-09-23 23:30:39 +0530

    faculty linkedin deleted

--- components/faculty.tsx
+++ components/faculty.tsx
@@ -7,19 +7,19 @@ export default function FacultyCoOrdinators() {
             <BioCards
                 img="https://bca.iem.edu.in/assets/images/FacultyPhoto/KS.jpg"
                 name="Prof. Kaustav Sarkar"
-                linkedin="https://www.linkedin.com/in/rohan-sinha-0b926225a/"
+                linkedin="https://www.linkedin.com/"
                 // role_="Logistics"
             />
             <BioCards
                 img="https://bca.iem.edu.in/assets/images/FacultyPhoto/Biswajit%20Maity.png"
                 name="Prof. Dr. Biswajit Maity"
-                linkedin="https://www.linkedin.com/in/rohan-sinha-0b926225a/"
+                linkedin="https://www.linkedin.com/"
                 // role_="Logistics"
             />
             <BioCards
                 img="https://bca.iem.edu.in/assets/images/FacultyPhoto/Rupam%20Bhattacharya.jpeg"
                 name="Prof. Dr. Rupam Bhattacharya"
-                linkedin="https://www.linkedin.com/in/rohan-sinha-0b926225a/"
+                linkedin="https://www.linkedin.com/"
                 // role_="Logistics"
             />
         </div>
