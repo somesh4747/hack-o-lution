15647eb4cd323af2f4130d87b87f6cc1277253ab
somesh4747 <somesh47.karmakar@gmail.com>
2024-10-25 12:14:00 +0530

    change for clg
    register is back

--- actions/test.ts
+++ actions/test.ts
@@ -3,13 +3,58 @@ import { sendSelectedTeamMail } from '@/utils/mail'
 import db from '@/utils/db'
 
 export async function testSelected() {
-    const teams = await db.team.findMany({
+    const teams = await db.member.findMany({
         select: {
-            mobile: true
-        }
+            email: true,
+            Team: {
+                select: {
+                    teamName: true,
+                },
+            },
+        },
+        skip: 15,
     })
-    console.log(teams)
-    // for (let i of teams) {
+    const mem = await db.team.findMany({
+        select: {
+            teamName: true,
+            mobile: true,
+        },
+    })
+    const lastMembers = [
+        {
+            email: 'boserudraksha007@gmail.com',
+            Team: { teamName: 'Coding Enthusiasts' },
+        },
+        {
+            email: 'suchismitasaha2006@gmail.com',
+            Team: { teamName: 'Coding Enthusiasts' },
+        },
+        {
+            email: 'sauranjalidhard@gmail.com',
+            Team: { teamName: 'Coding Enthusiasts' },
+        },
+        {
+            email: 'soumyadipadak99@gmail.com',
+            Team: { teamName: 'TriCore Innovators' },
+        },
+        {
+            email: 'srijanray11@gmail.com',
+            Team: { teamName: 'TriCore Innovators' },
+        },
+        {
+            email: 'shashwatasuvro@gmail.com',
+            Team: { teamName: 'TriCore Innovators' },
+        },
+        {
+            email: 'souravbose361@gmail.com',
+            Team: { teamName: 'Debugging Ninjas' },
+        },
+        {
+            email: 'sourishchkbkp@gmail.com',
+            Team: { teamName: 'Debugging Ninjas' },
+        },
+    ]
+    // for (let i of lastMembers) {
     //     if (!i.Team) {
     //         return
     //     }
---
+++ app/register/__page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Register_page() {
+    return (
+        <div className="h-screen flex justify-center items-center text-6xl text-center">
+            Registration Closed
+        </div>
+    )
+}
